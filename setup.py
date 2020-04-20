1177ea1991f93128e8ab4f3ad79495f7234369ed
NBanski <n.d.banski@gmail.com>
2020-04-20 12:23:26 +0200

    tweaks

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="recatcher",
-    version='1.0.0',
+    version='1.1',
+    author='Norbert Bański',
+    author_email='n.d.banski@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
